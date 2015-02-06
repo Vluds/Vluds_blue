83885511f3625892744fda9d985b25543bdbc6f4
mxcmaxime <join.mxcmaxime@outlook.fr>
2015-02-07 01:28:55 +0300

    .

--- models/manager.php
+++ models/manager.php
@@ -9,12 +9,38 @@ if(User::isLogged()) {
 		<table>
 			<tr>
 				<th>ID</th>
-				<th>User</th>
+				<th>User ID</th>
 				<th>Content</th>
 				<th>Action</th>
 			</tr>
-			<tr><td></td></tr>
-		</table>
+	<?php
+		$PublicationInfos = $newStaticBdd->select("*", "publications", "");
+        while ($getPublicationInfos = $newStaticBdd->fetch_array($PublicationInfos)) {
+
+        	if( strlen($getPublicationInfos['content']) >= 40 ) {
+
+				$chaine = substr($getPublicationInfos['content'],0,40);
+
+			} else {
+
+				$chaine = $getPublicationInfos['content'];
+
+			}
+
+    ?>
+			<tr>
+				<td><?php echo $getPublicationInfos['id']; ?></td>
+				<td><?php echo $getPublicationInfos['user_id']; ?></td>
+				<td><?php echo $chaine; ?></td>
+				<td><button onClick="deletePublication(<?php echo $getPublicationInfos['id']; ?>)">DELETE</button></td>
+			</tr>
+
+	<?php
+
+		}
+
+	?>
+	</table>
 	<?php
 
 	}
